16fe73a20cafdc32670901917f71c98af115047f
Mau <79354255+Luceium@users.noreply.github.com>
2023-11-09 01:30:44 -0800

    added testing configurations

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("test");
+  let x = 1;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
     </View>
   );
 }
