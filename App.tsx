50a99432dbb7347433e2c1bbff65dbc4f8bac2a7
Mau <79354255+Luceium@users.noreply.github.com>
2023-11-20 00:20:48 -0800

    starting proj

--- App.tsx
+++ App.tsx
@@ -3,26 +3,15 @@ const classNames = require('classnames');
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={() => console.log("text clicked")}>Hello World! Lorem Ipsum Doler Sit Ammet</Text>
-      <TouchableNativeFeedback onPress={() => Alert.alert("Warning", "Please don't click on the image", [
-        {text: "Ok", onPress: () => console.log("Thank you")},
-        {text: "Too bad", onPress: () => BackHandler.exitApp()}
-      ])}>
-        <View style={{height: 300, width: 300, justifyContent: "center", alignItems: "center", display: "flex", borderRadius: 10}}>
-          <Image source={{uri: "https://picsum.photos/200/200", width:200, height:200}} blurRadius={10} fadeDuration={300} />
-        </View>
-      </TouchableNativeFeedback>
-    </SafeAreaView>
+    <View style={styles.container}>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "dodgerblue",
     flex: 1,
-    backgroundColor: '#ffc',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
