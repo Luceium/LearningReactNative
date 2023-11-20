1e433597e6cee72d06bf0fc3acb7e183ae8fd41b
Mau <79354255+Luceium@users.noreply.github.com>
2023-11-20 00:29:17 -0800

    test flex

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@ const classNames = require('classnames');
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <View style={{backgroundColor: "#fcc", flex:4}} />
+      <View style={{backgroundColor: "#ffc", flex:3}} />
+      <View style={{backgroundColor: "#cff", flex:2}} />
+      <View style={{backgroundColor: "#ccf", flex:1}} />
     </View>
   );
 }
@@ -12,6 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "dodgerblue",
+    flexDirection: "row-reverse",
     flex: 1,
   },
 });
